33b5c37236e33505edf7c62cbbf393303f2f9aaf
shash29exe <zzxc67643@gmail.com>
2025-08-11 14:43:48 +0500

    Возврат в главное меню. / Back to main menu.

--- handlers/h03_order.py
+++ handlers/h03_order.py
@@ -1,15 +1,17 @@
 from aiogram import Router, F, Bot
 from aiogram.types import Message
 
+from handlers.h02_contact_user import get_main_menu
 from keyboards.inline_kb import get_category_menu
 from keyboards.reply_kb import back_to_main_menu
 
 router = Router()
 
+
 @router.message(F.text == '📝 Оформить заказ')
-async def make_order(message: Message, bot):
+async def make_order(message: Message, bot: Bot):
     """
-        реакция на кнопку '📝 Оформить заказ', редоставление категорий товаров
+        реакция на кнопку '📝 Оформить заказ', предоставление категорий товаров
     """
 
     chat_id = message.chat.id
@@ -18,3 +20,26 @@ async def make_order(message: Message, bot):
                            text="Оформляем заказ",
                            reply_markup=back_to_main_menu())
     await message.answer(text="Выберете категорию", reply_markup=get_category_menu(chat_id))
+
+
+@router.message(F.text == '🏠 Главное меню')
+async def h_main_menu(message: Message, bot: Bot):
+    """
+        реакция на кнопку '🏠 Главное меню'
+    """
+
+    await bot.delete_message(chat_id=message.chat.id,
+                             message_id=message.message_id - 1)
+
+    await get_main_menu(message)
+
+
+@router.message(F.text == '📖 История заказов')
+async def h_history_orders(message: Message):
+    """
+        реакция на кнопку '📖 История заказов'
+    """
+
+    chat_id = message.chat.id
+
+
