b00988e27f2136bf87369424e4b9e1e1cef9882b
shash29exe <zzxc67643@gmail.com>
2025-09-16 12:38:03 +0500

    Меню подтверждения удаленя аккаунта. / Confirmation menu for account deletion.

--- handlers/h12_settings.py
+++ handlers/h12_settings.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F, Bot
 from aiogram.types import CallbackQuery, Message
 
-from keyboards.inline_kb import settings_kb
+from keyboards.inline_kb import settings_kb, confirm_delete_kb
 
 router = Router()
 
@@ -12,3 +12,14 @@ async def settings(message: Message):
     """
 
     await message.answer(text="⚙ Настройки", reply_markup=settings_kb())
+
+async def change_language():
+    pass
+
+@router.callback_query(F.data == 'delete_account')
+async def delete_account(callback: CallbackQuery):
+    """
+        Удаление аккаунта
+    """
+
+    await callback.message.edit_text("Подтвердите удаление аккаунта", reply_markup=confirm_delete_kb())
