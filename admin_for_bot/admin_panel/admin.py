b67b75e0a177955e99dce82da46d13f8543945c1
shash29exe <zzxc67643@gmail.com>
2025-09-25 12:17:16 +0500

    Добавление всех таблиц и регистрация их. /
    Добавление всех таблиц и регистрация их.

--- admin_for_bot/admin_panel/admin.py
+++ admin_for_bot/admin_panel/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Users
+from .models import Users, Categories, Products
+
 
 @admin.register(Users)
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'telegram', 'phone')
+    search_fields = ('name', 'phone')
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
+    search_fields = ('category_name', )
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'price', 'category')
+    search_fields = ('product_name', 'price')
+    list_filter = ('category', )
