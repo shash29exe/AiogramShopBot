9f513d5784be2449562fe3d8bc2543a565201259
shash29exe <zzxc67643@gmail.com>
2025-10-04 12:50:37 +0500

    meida upload fix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-TOKEN = getenv('TOKEN')
+TOKEN = os.getenv('TOKEN')
 MANAGER_ID = int(os.getenv('MANAGER_ID'))
 MANAGER_USERNAME = os.getenv('MANAGER_USERNAME')
-MEDIA_ROOT = r'C:\Users\Shash29\PycharmProjects\AiogramShopBot'
+MEDIA_ROOT = r'C:\Users\Shash29\PycharmProjects\AiogramShopBot\media'
